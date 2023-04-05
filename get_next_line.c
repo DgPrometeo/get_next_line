10e9175ce8d91b121aca499d6a61d1d82218a18e
danielga <danielga@student.42malaga.com>
2023-04-05 21:06:16 +0200

    Modificacion de funcion principal
    Gracias a una sesión eexplicativa del ejercicio he reflexionado el
    ejercicio y voy a seguir otro rumbo, pero guardaré el anterior como una
    versión previa.

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,34 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 10:52:18 by danielga          #+#    #+#             */
-/*   Updated: 2023/03/30 16:41:58 by danielga         ###   ########.fr       */
+/*   Updated: 2023/04/05 20:52:15 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*get_read(char *buffer, int fd)
 {
-	char	*line;
-	int		i;
 	int		r;
+	char	*tmp;
 
-	i = 0;
-	r = 0;
-	line = malloc(sizeof(char) * BUFFER_SIZE);
-	if (!line)
-		return (NULL);
-	while ((r = read(fd, &line[i], 1)) > 0)
+	r = 1;
+	tmp = ft_calloc_bzero(BUFFER_SIZE, sizeof(char));
+	while (r > 0)
 	{
-		if (line[i] == '\n')
+		r = read(fd, tmp, BUFFER_SIZE);
+		buffer = ft_strjoin(buffer, tmp);
+		if (ft_strchr(buffer, '\n'))
 			break;
-		i++;
 	}
-	if (r == 0 && i == 0)
-	{
-		free(line);
-		return (NULL);
-	}
-	line[i] = '\0';
-	return (line);
+	free(tmp);
+	return (buffer);
 }
 
 /*
-Creamos una reserva de memoria que recoja la información de lo que vaya a
-guardar y no ha leido, para despues seguir leyéndolo. 
+Función para leer el archivo. 
 
-Si lee la función debe de dar un número superior a 0 para poder continuar.
-
+necesitaremos un entero que sea nuestro valor de lectura para la función READ;
 READ devuelve el numero de bytes leidos. tiene que ser menor que buffer_size.
 ssize_t read (int fd, void *buf, size_t count): 
 -fd: el file descriptor que leerá.
@@ -54,7 +44,25 @@ Devuelve:
 -Si fd no es valido devuelve -1 estableciendo "errno" en "EBADF".
 -Si el buffer es nulo o mayor al Int_max devolverá -1.
 
+y un string temporal que 
+*/
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*buffer;
+
+	buffer = 0;
+	line = get_read(buffer, fd);
+	return (line);
+}
+
+/*
+Creamos una reserva de memoria que recoja la información de lo que vaya a
+guardar y no ha leido, para despues seguir leyéndolo. 
+
+Si lee la función debe de dar un número superior a 0 para poder continuar.
+
 Finalmente, la línea se habrá formado, por lo que añadimos en la posición
 saliente el nulo y devolvemos la línea leída.
-
 */
